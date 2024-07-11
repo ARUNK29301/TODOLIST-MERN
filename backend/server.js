6a50ee246b03b73c29f4e774b0d8a59678d61908
ARUNK29301 <128120791+ARUNK29301@users.noreply.github.com>
2024-07-11 14:29:31 +0530

    backend updated

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const crudRoutes = require('./routes/crud');
+
+const app = express();
+const MONGODBURL = "mongodb+srv://arunkandhasamy29:123asd@cluster0.xlnibjs.mongodb.net/todotest?retryWrites=true&w=majority&appName=Cluster0";
+app.use(express.json());
+
+app.use((req, res, next) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+app.use(crudRoutes);
+
+app.use((error, req, res, next) => {
+
+    console.log(error);
+    res.status(error.statusCode || 500).json({
+        message: error.message,
+        data: error.data
+    });
+
+});
+
+mongoose.connect(MONGODBURL)
+    .then(result => {
+        console.log("first")
+        app.listen(5001);
+    })
+    .catch(error => {
+        console.log(error);
+    });
