657e77b2c770f53168668ec71b68b6f2591085a7
ARUNK29301 <128120791+ARUNK29301@users.noreply.github.com>
2024-07-11 12:50:18 +0530

    build file

---
+++ client/build/precache-manifest.1a5fcc89db9eca9c3e2060302e24f233.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a224ee3aa05defcb8972bae2010d39c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3987b20771ee69210fc",
+    "url": "/static/css/main.7027387f.chunk.css"
+  },
+  {
+    "revision": "09f0c1705b4f9800b1d7",
+    "url": "/static/js/2.dea36e86.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.dea36e86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3987b20771ee69210fc",
+    "url": "/static/js/main.969c179b.chunk.js"
+  },
+  {
+    "revision": "4a29157b3354189a7920",
+    "url": "/static/js/runtime-main.3e20e9b1.js"
+  }
+]);
